ff08e21be6cdd4aaa5df0d7c4b86529be8cd1905
Jeevan Boyeni <jeevanb214@gmail.com>
2020-03-07 18:19:04 +0530

    1 st commit for more info google docs

---
+++ KafkaProducer/src/main/java/config/KakfaConfiguration.java
@@ -0,0 +1,39 @@
+package config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import model.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KakfaConfiguration {
+
+    @Bean
+    public ProducerFactory<String, User> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+		/*
+		 * config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+		 * StringSerializer.class);
+		 * config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+		 * JsonSerializer.class);
+		 *////we can also give the above configuration in the properties file
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+
+    @Bean
+    public KafkaTemplate<String, User> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+
+}
---
+++ KafkaProducer/src/main/java/model/User.java
@@ -0,0 +1,39 @@
+package model;
+
+public class User {
+
+	String name;
+	String tech;
+    Long salary;
+
+
+	public User(String name, String string, Long salary) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTech() {
+		return tech;
+	}
+
+	public void setTech(String tech) {
+		this.tech = tech;
+	}
+
+	public Long getSal() {
+		return salary;
+	}
+
+	public void setSal(Long salary) {
+		this.salary = salary;
+	}
+
+	
+}
